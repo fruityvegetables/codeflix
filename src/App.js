e2e101b31da54ba4688d2654f5992595170be6d0
fruityvegetables <gg.troyer@gmail.com>
2021-01-16 15:24:52 -0600

    fixed a typo in axios call; updated Row component to show an example API call

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>HEllo and LET'S MAKE A NETFLIX RIPOFF!</h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending now"/>
-      
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> parent of 8e8f348... added a requests.js file for different API requests, and setup env for API_KEY
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    //run a piece of code when row loads
+    // if the brackets are empty, it will only run once when the page loads
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
+        }
+        fetchData();
+    }, []);
     return (
         <div>
             <h2>{title}</h2>
