135d2df03db1935065be2d643da7f265e38eb663
fruityvegetables <gg.troyer@gmail.com>
2021-01-17 04:44:45 -0600

    added style for the banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import "./Banner.css";
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -20,6 +21,10 @@ function Banner() {
     }, []);
 
     console.log(movie);
+    //truncates the text description
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    }
 
     return (
         <header className="banner"
@@ -31,7 +36,7 @@ function Banner() {
             backgroundPosition: "center center",
         }}>
             <div className="banner_contents">
-                <h1>
+                <h1 className="banner_title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner_buttons">
@@ -39,7 +44,7 @@ function Banner() {
                     <button className="banner_button">My List</button>
                 </div>
                 <h1 className="banner_description">
-                    {movie?.overview}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
         </header>
