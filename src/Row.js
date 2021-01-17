843e2393428f69cbcdff7f391ab2013d0b2464e6
fruityvegetables <gg.troyer@gmail.com>
2021-01-17 03:05:49 -0600

    added an API call that returns giant movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
@@ -9,15 +11,26 @@ function Row({ title, fetchUrl }) {
     useEffect(() => {
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            console.log(request.data.results);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.log(movies);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-            {/* {posters} */}
+            <div className="row_posters">
+                {/* {row_posters} */}
+
+                {movies.map(movie => {
+                    return <img src={`${base_url}${movie.poster_path}`}
+                    alt={movie.name}/>
+                })}
+            </div>
         </div>
     )
 }
